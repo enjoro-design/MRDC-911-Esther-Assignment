5dd47d31fd05ec976e3a31dad5200cef1f34e628
enjoro-design <njorogeesther0@gmail.com>
2025-06-12 12:49:42 +0300

    Updated

---
+++ Codes for each question/Question 9.R
@@ -0,0 +1,4 @@
+missing_summary <- sapply(data, function(x) sum(is.na(x)) / length(x) * 100)
+missing_summary[missing_summary > 0]
+missing_summary <- sapply(data, function(x) sum(is.na(x)) / length(x) * 100)  
+missing_summary[missing_summary > 0]  
